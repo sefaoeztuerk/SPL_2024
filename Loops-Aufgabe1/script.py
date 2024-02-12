4b73a7b4c414f791c1e7563ed2bdf69396d169cc
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2024-02-12 10:09:19 +0100

    Loop Aufgabe 1 done

---
+++ Loops-Aufgabe1/script.py
@@ -0,0 +1,10 @@
+#Zähle in einem For-Loop die Zahlen von 1 bis 100 (inklusive) zusammen
+
+summe = 0
+
+for zahl in range(1, 101):
+
+    summe += zahl
+
+
+print("Die Summe der Zahlen von 1 bis 100 ist:", summe)
